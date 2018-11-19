af251ac246dde362717e837eaa897c3ca66907f8
Francesco Colombo <francesco.colombo@admaiorastudio.com>
2018-11-19 19:41:31 +0100

    First Commit

---
+++ Android/Android/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Android.App;
+using Android.Graphics;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using Com.Bumptech.Glide;
+using Java.Lang;
+
+namespace Android
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            ImageView imageView = FindViewById<ImageView>(Resource.Id.MyImageView);
+            Glide.With(this).AsGif().Load(Resource.Drawable.gattino).Into(imageView);           
+        }
+    }
+}
+
---
+++ iOS/iOS/ViewController.cs
@@ -0,0 +1,40 @@
+﻿using CoreGraphics;
+using Foundation;
+using SDWebImage;
+using System;
+using UIKit;
+
+namespace iOS
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController (IntPtr handle) 
+            : base (handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            nfloat centerX = this.View.Center.X - 100;
+            nfloat centerY = this.View.Center.Y - 100;
+
+            FLAnimatedImage animatedImage = FLAnimatedImage.AnimatedImageWithGIFData(
+                NSData.FromFile(NSBundle.MainBundle.PathForResource("gattino", "gif")));
+
+            FLAnimatedImageView imageView = new FLAnimatedImageView();
+            imageView.AnimatedImage = animatedImage;
+            imageView.Frame = new CGRect(centerX, centerY, 200, 200);
+
+            this.View.AddSubview(imageView);
+
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning ();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
